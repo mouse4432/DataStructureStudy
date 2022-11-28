0f0594a76177c389693f1fd0488b71b44888fc06
mouse4432 <88102469+mouse4432@users.noreply.github.com>
2022-11-28 18:27:28 +0900

    week4 is done

---
+++ 210531/pb1.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct reportCard{
+    char name[20];
+    int score;
+}RC;
+
+RC S[100];
+
+
+void merge2(int low, int mid, int high){
+    int i = low, j=mid+1, k = 0;
+    int idx = high-low+1;
+    RC U[100];
+    while(i<=mid && j<=high)
+        U[k++] = (S[i].score > S[j].score) ? S[i++] : S[j++];
+    if(i>mid)
+        while(j<=high)
+            U[k++] = S[j++];
+    else
+        while(i<=mid)
+            U[k++] = S[i++];
+    
+    for(int t = low; t<=high; t++)
+        S[t] = U[t-low];
+
+   
+}
+
+void mergesort2(int low, int high)
+{
+    if(low<high){
+        int mid = (low+high)/2;
+        mergesort2(low, mid);
+        mergesort2(mid+1, high);
+        merge2(low, mid, high);
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++)
+        scanf("%s %d", S[i].name, &S[i].score);
+      
+    printf("before merge sort>>\niterative\n");
+    for(int i=0; i<n; i++)
+        printf("%2s:  %2d\n", S[i].name, S[i].score);
+     
+    mergesort2(0, n-1);
+   
+    printf("\n\nafter merge sort>>\niterative\n");
+    for(int i=0; i<n; i++)
+        printf("%2s:  %2d\n", S[i].name, S[i].score);
+}
---
+++ 210531/pb2.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct reportCard{
+    char name[20];
+    int score;
+}RC;
+
+RC S[100];
+int n;
+int pivot;
+
+int static count;
+
+
+void partition(int low, int high){
+    //swap(S[low], S[high]);
+    int pivotitem = S[low].score;
+    RC tmp;
+
+    int j = low;
+    for(int i=low+1; i<=high; i++){
+        if(S[i].score<pivotitem){
+            j++;
+            tmp;
+            tmp = S[i];
+            S[i] = S[j];
+            S[j] = tmp;
+            
+        }
+    }
+    pivot = j;
+    tmp = S[low];
+    S[low] = S[pivot];
+    S[pivot] = tmp;
+}
+
+void quicksort(int low, int high){  
+    if(low<high){
+        partition(low, high);
+        quicksort(low, pivot-1);
+        quicksort(pivot+1, high);
+    }
+}
+
+int main()
+{
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+        scanf("%s %d", S[i].name, &S[i].score);
+
+    printf("before quick sort>>\n");
+    for(int i=1; i<=n; i++)
+        printf("%2s:  %2d\n", S[i].name, S[i].score);
+   
+    quicksort(1, n);
+    
+    printf("\nafter quick sort>>\n");
+    for(int i=1; i<=n; i++)
+        printf("%2s:  %2d\n", S[i].name, S[i].score);   
+}
